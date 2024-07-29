73d54fa393d8737da5599ad29aa0d0d31007bc1d
Haseeb30000 <34556583+Haseeb30000@users.noreply.github.com>
2024-07-29 14:57:49 +0500

    sushiswap

--- services/decoder/protocols/sushiswap/sushiswap-v2.configs.ts
+++ services/decoder/protocols/sushiswap/sushiswap-v2.configs.ts
@@ -13,8 +13,202 @@ const configs: Configs = [
         protocol_name: "sushiswap-v2",
         chain_name: "eth-mainnet",
     },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "arbitrum-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "arbitrum-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "avalanche-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "avalanche-mainnet",
+    },
+
+    {
+        address: "0x71524B4f93c58fcbF659783284E38825f0622859", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "base-mainnet",
+    },
+    {
+        address: "0x6BDED42c6DA8FBf0d2bA55B2fa120C5e0c8D7891", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "base-mainnet",
+    },
+
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "boba-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "boba-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "bsc-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "bsc-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "celo-mainnet",
+    },
+    {
+        address: "0x1421bDe4B10e8dd459b3BCb598810B1337D56842", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "celo-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "fantom-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "fantom-mainnet",
+    },
+
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "harmony-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "harmony-mainnet",
+    },
+
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "heco-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "heco-mainnet",
+    },
+    {
+        address: "0x580ED43F3BBa06555785C81c2957efCCa71f7483", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "metis-mainnet",
+    },
+    {
+        address: "0xbF3B71decBCEFABB3210B9D8f18eC22e0556f5F0", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "metis-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "moonbeam-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "moonbeam-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "moonbeam-moonriver",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "moonbeam-moonriver",
+    },
+    {
+        address: "0xFbc12984689e5f15626Bad03Ad60160Fe98B303C", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "optimism-mainnet",
+    },
+    {
+        address: "0x2ABf469074dc0b54d793850807E6eb5Faf2625b1", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "optimism-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "matic-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "matic-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "palm-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "palm-mainnet",
+    },
+    {
+        address: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", //Factory
+        is_factory: true,
+        protocol_name: "sushiswap-v2",
+        chain_name: "telos-mainnet",
+    },
+    {
+        address: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", //Router
+        is_factory: false,
+        protocol_name: "sushiswap-v2",
+        chain_name: "telos-mainnet",
+    },
 ];
-
-
 
 export default configs;
